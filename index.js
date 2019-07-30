daff4d218069e5e546564e674fe3b539b010cd64
kabo <28654659+kabo2468@users.noreply.github.com>
2019-07-31 01:43:37 +0900

    update

--- index.js
+++ index.js
@@ -39,12 +39,14 @@ ws.addEventListener('close', function() {
 ws.addEventListener('message', function(data){
     console.log('----------Start----------');
     const json = JSON.parse(data.data);
-    console.dir(json);
 
     if (json.body.id === '1803ad27-a839-4eb6-ac74-97677ee0a055') { //Timeline
+        console.dir(json);
+
         if (json.body.body.userId === process.env.USER_ID) return;
         let text = json.body.body.text;
         if (text === null) return;
+        if (/@oishiibot/.test(text)) return;
 
         //URLを消す
         text = text.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=@]*)?/g, '');
@@ -96,6 +98,8 @@ ws.addEventListener('message', function(data){
         if (json.body.type === 'notification') return;
 
         if (json.body.type === 'followed') { //follow back
+            console.dir(json);
+
             ws.send(JSON.stringify({
                 type: 'api',
                 body: {
@@ -109,11 +113,15 @@ ws.addEventListener('message', function(data){
         }
 
         if (json.body.type === 'mention') {
+            console.dir(json);
+
             if (json.body.body.user.isBot === true) return;
 
             let text = json.body.body.text;
             if (text === null) return;
             text = text.replace(/http(s)?:\/\/([\w-]+\.)+[\w-]+(\/[\w- ./?%&=@]*)?/g, '');
+            text = text.replace('@oishiibot', '');
+            console.log(`json text:${text}`);
 
             const note_id = json.body.body.id;
             const reaction_data = {
@@ -129,67 +137,80 @@ ws.addEventListener('message', function(data){
             };
             ws.send(JSON.stringify(reaction_data));
 
-            let m = text.match(/(.+)(は|って)(おいしい|美味しい|まずい|不味い)[？?]+/g);
+            let m = text.match(/(.+)(は|って)(おいしい|美味しい|まずい|不味い)[？?]+/);
             if (m) { // check
-                if (is_noun(m[1])) {
-                    let is_good = false;
-                    const query = {
-                        text: 'SELECT good FROM oishii_table WHERE name=$1',
-                        values: [ m[1] ]
-                    };
-                    client.query(query)
-                    .then(res => {
-                        if (res.rows[0].length < 1) {
-                            throw 'Not found';
-                        }
-                        is_good = res.rows[0].good;
-                        console.log(is_good);
-
-                        const text = is_good ? 'おいしい' : 'まずい';
-                        reply(text, note_id);
-                    })
-                    .catch(e => {
-                        console.log(e);
-                        reply('わからない', note_id);
-                    });
-                } else {
-                    reply('それ食べれる？', note_id);
-                }
-            } 
-            m = text.match('(.+)[はも](おいしい|美味しい|まずい|不味い)よ?[！!]*', 'g');
-            if (m) { // learn
-                if (is_noun(m[1])) {
-                    const is_good = m[2].match(/(おいしい|美味しい)/) ? true : false;
-
-                    get_exists(m[1])
-                    .then(res => {
-                        if (res === true) {
-                            const update_query = {
-                                text: 'UPDATE oishii_table SET good=$1, learned=true WHERE name=$2',
-                                values: [ is_good, m[1] ]
-                            };
-                            client.query(update_query)
-                            .then(res => console.log(res))
-                            .catch(e => console.error(e.stack));
-                        } else {
-                            const add_query = {
-                                text: 'INSERT INTO oishii_table ( name, good, learned ) VALUES ( $1, $2, true )',
-                                values: [ add_name, is_good ]
-                            };
-                            client.query(add_query)
-                            .then(res => console.log(res))
-                            .catch(e => console.error(e.stack));
-                        }
-                    }).then(() => {
-                        reply(`${m[1]}は${m[2]}\nおぼえた`, note_id);
-                    });
-                } else {
-                    reply('それ食べれる？', note_id);
-                }
+                new Promise((resolve, reject) => {
+                    console.log(is_noun(m[1]));
+                    resolve(is_noun(m[1]));
+                })
+                .then(res => {
+                    if (res) {
+                        let is_good = false;
+                        const query = {
+                            text: 'SELECT good FROM oishii_table WHERE name=$1',
+                            values: [ m[1] ]
+                        };
+                        client.query(query)
+                        .then(res => {
+                            if (res.rows[0].length < 1) {
+                                throw 'Not found';
+                            }
+                            is_good = res.rows[0].good;
+                            console.log(is_good);
+    
+                            const text = is_good ? 'おいしい' : 'まずい';
+                            reply(text, note_id);
+                        })
+                        .catch(e => {
+                            console.log(e);
+                            reply('わからない', note_id);
+                        });
+                    } else {
+                        reply('それ食べれる？', note_id);
+                    }
+                });
+                return;
             }
-            m = text.match('(おいしい|美味しい|まずい|不味い)(もの|物|の)は[？?]*', 'g');
+            m = text.match('(.+)[はも](おいしい|美味しい|まずい|不味い)よ?[！!]*');
+            if (m) { // learn
+                new Promise((resolve, reject) => {
+                    resolve(is_noun(m[1]));
+                })
+                .then(res => {
+                    if (res) {
+                        const is_good = m[2].match(/(おいしい|美味しい)/) ? true : false;
+    
+                        get_exists(m[1])
+                        .then(res => {
+                            if (res === true) {
+                                const update_query = {
+                                    text: 'UPDATE oishii_table SET good=$1, learned=true WHERE name=$2',
+                                    values: [ is_good, m[1] ]
+                                };
+                                client.query(update_query)
+                                .then(res => console.log(res))
+                                .catch(e => console.error(e.stack));
+                            } else {
+                                const add_query = {
+                                    text: 'INSERT INTO oishii_table ( name, good, learned ) VALUES ( $1, $2, true )',
+                                    values: [ add_name, is_good ]
+                                };
+                                client.query(add_query)
+                                .then(res => console.log(res))
+                                .catch(e => console.error(e.stack));
+                            }
+                        }).then(() => {
+                            reply(`${m[1]}は${m[2]}\nおぼえた`, note_id);
+                        });
+                    } else {
+                        reply('それ食べれる？', note_id);
+                    }
+                });
+                return;
+            }
+            m = text.match('(おいしい|美味しい|まずい|不味い)(もの|物|の)は[？?]*');
             if (m) { // search
-
+                return;
             }
         }
     }
@@ -283,6 +304,7 @@ function get_exists(text) {
 }
 
 function is_noun(text) {
+    console.log(`is_noun text: ${text}`);
     builder.build((err, tokenizer) => {
         if (err) throw err;
 
@@ -292,7 +314,7 @@ function is_noun(text) {
             return token.pos === '名詞' && token.pos_detail_1 !== 'サ変接続' ? token.surface_form : null;
         });
         const nouns = pos_arr.filter(n => n !== null);
-        // console.log(`nouns: ${nouns}`);
+        console.log(`nouns: ${nouns}`);
         //もし何もなかったら
         if (nouns.length < 1) return false;
 
