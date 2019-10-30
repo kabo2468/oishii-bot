6669ed8747afa7db8791bcf18422e1c5c0c9ab15
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2019-10-30 22:38:50 +0900

    Add visibility

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ ws.addEventListener('message', function(data){
         if (text === null) return;
         if (json.body.body.cw !== null) return;
         if (/@oishiibot/.test(text)) return;
+        if (json.body.body.visibility === 'specified') return;
 
         // heroku DB 制限
         psql.query('SELECT count(*) FROM oishii_table').then(res => {
@@ -175,6 +176,7 @@ ws.addEventListener('message', function(data){
             console.log(`json text:${text}`);
 
             const note_id = json.body.body.id;
+            const visibility = json.body.body.visibility;
             const reactionData = {
                 type: 'api',
                 body: {
@@ -186,7 +188,7 @@ ws.addEventListener('message', function(data){
                     }
                 }
             };
-            if (text.match(/^\s*かわいい*[！!]*\s*$/)) reactionData.body.data.reaction = 'love';
+            if (text.match(/^\s*かわい{2,}[！!]*\s*$/)) reactionData.body.data.reaction = 'love';
             ws.send(JSON.stringify(reactionData));
 
             let m;
@@ -194,13 +196,13 @@ ws.addEventListener('message', function(data){
             m = text.match(/^\s*\/help\s*$/);
             if (m) { // help
                 console.log('COMMAND: help');
-                sendText({text: messages.commands.help, reply_id: note_id});
+                sendText({text: messages.commands.help, reply_id: note_id, visibility: visibility});
                 return;
             }
             m = text.match(/^\s*\/ping\s*$/);
             if (m) { // ping
                 console.log('COMMAND: ping');
-                sendText({text: messages.commands.ping, reply_id: note_id});
+                sendText({text: messages.commands.ping, reply_id: note_id, visibility: visibility});
                 return;
             }
             m = text.match(/^\s*\/info\s*$/);
@@ -212,7 +214,7 @@ ws.addEventListener('message', function(data){
                     const all = Number(fl) + Number(tl);
                     const text = `Records: ${all.toString()} (Learned: ${tl})`;
                     console.log(`COMMAND: info[ ${text} ]`);
-                    sendText({text: text, reply_id: note_id});
+                    sendText({text: text, reply_id: note_id, visibility: visibility});
                 });
                 return;
             }
@@ -222,7 +224,7 @@ ws.addEventListener('message', function(data){
                 if (json.body.body.user.username === 'kabo') {
                     sayFood();
                 } else {
-                    sendText({text: messages.commands.denied, reply_id: note_id});
+                    sendText({text: messages.commands.denied, reply_id: note_id, visibility: visibility});
                 }
                 return;
             }
@@ -242,9 +244,9 @@ ws.addEventListener('message', function(data){
                         if (res.rowCount < 1) {
                             isNoun(text).then(is_noun => {
                                 if (is_noun) {
-                                    sendText({text: messages.food.idk, reply_id: note_id});
+                                    sendText({text: messages.food.idk, reply_id: note_id, visibility: visibility});
                                 } else {
-                                    sendText({text: messages.food.canEat, reply_id: note_id});
+                                    sendText({text: messages.food.canEat, reply_id: note_id, visibility: visibility});
                                 }
                             });
                             return;
@@ -252,7 +254,7 @@ ws.addEventListener('message', function(data){
                         const isGood = res.rows[0].good;
                         const goodS = isGood ? messages.food.good : messages.food.bad;
                         console.log(`CHECK: ${text}`);
-                        sendText({text: goodS, reply_id: note_id});
+                        sendText({text: goodS, reply_id: note_id, visibility: visibility});
                     })
                     .catch(e => console.log(e));
                 })();
@@ -281,18 +283,20 @@ ws.addEventListener('message', function(data){
                             .then(() => console.log(`LEARN(INSERT): ${text} is ${is_good}`))
                             .catch(e => console.error(e.stack));
                     }
-                    sendText({text: `${text}${messages.food.is}${m[2]}\n${messages.food.learn}`, reply_id: note_id});
+                    sendText({text: `${text}${messages.food.is}${m[2]}\n${messages.food.learn}`, reply_id: note_id, visibility: visibility});
                 })();
                 return;
             }
-            m = text.match(`(${variables.food.good}|${variables.food.bad})(もの|物|の)は?(何|なに)?[？?]*`);
+            m = text.match(`(みんなの)?(${variables.food.good}|${variables.food.bad})(もの|物|の)は?(何|なに)?[？?]*`);
             if (m) { // search
                 (async () => {
-                    const is_good = m[1].match(`(${variables.food.good})`) ? true : false;
+                    const is_good = m[2].match(`(${variables.food.good})`) ? true : false;
                     const search_query = {
                         text: 'SELECT name FROM oishii_table WHERE good=$1',
                         values: [is_good]
                     };
+                    if (m[1]) search_query.text = 'SELECT name FROM oishii_table WHERE good=$1 AND learned=true';
+
                     psql.query(search_query)
                         .then(res => {
                             // console.dir(res);
@@ -300,7 +304,7 @@ ws.addEventListener('message', function(data){
                             // console.dir(row);
                             const igt = is_good ? messages.food.good : messages.food.bad;
                             console.log(`SEARCH: ${row.name} (${is_good})`);
-                            sendText({text: `${row.name}${messages.food.is}${igt}`, reply_id: note_id});
+                            sendText({text: `${row.name}${messages.food.is}${igt}`, reply_id: note_id, visibility: visibility});
                         })
                         .catch(e => console.error(e.stack));
                 })();
@@ -322,7 +326,7 @@ ws.addEventListener('message', function(data){
                             const good = row.match(re)[2];
                             const text = `${name} とかどう？\n${good === 't' ? messages.food.good : messages.food.bad}よ`;
                             console.log(`HUNGRY: ${text}`);
-                            sendText({text: text, reply_id: note_id});
+                            sendText({text: text, reply_id: note_id, visibility: visibility});
                         })
                         .catch(e => console.error(e.stack));
                 })();
@@ -356,7 +360,7 @@ function sayFood() {
         .catch(e => console.error(e.stack));
 }
 
-function sendText({text, reply_id, visibility = 'public'}) {
+function sendText({text, reply_id, visibility = 'public', user_id}) {
     const sendData = {
         type: 'api',
         body: {
@@ -371,6 +375,10 @@ function sendText({text, reply_id, visibility = 'public'}) {
         }
     };
     if (reply_id) sendData.body.data.replyId = reply_id;
+    if (user_id) {
+        sendData.body.data.visibility = 'specified';
+        sendData.body.data.visibleUserIds = user_id;
+    }
     ws.send(JSON.stringify(sendData));
 }
 
