dc136e887495c419ad861acc0df9f3384a35e18a
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2019-10-31 21:35:04 +0900

    Fix regex

--- index.js
+++ index.js
@@ -288,7 +288,7 @@ ws.addEventListener('message', function(data){
                 })();
                 return;
             }
-            m = text.match(`(みん(な|にゃ)の)?(${variables.food.good}|${variables.food.bad})(もの|物|の)は?(何|なに)?[？?]*`);
+            m = text.match(`(みん(な|にゃ)の)?(${variables.food.good}|${variables.food.bad})(もの|物|の)は?(何|(な|にゃ)に)?[？?]*`);
             if (m) { // search
                 (async () => {
                     const is_good = m[3].match(`(${variables.food.good})`) ? true : false;
