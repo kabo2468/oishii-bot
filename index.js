f1af26ad8993924f4d99921ecd6804b6a070e2a4
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-01-21 15:41:01 +0900

    uuidをnpmの方を使うようにした

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ if (error.length > 0) {
 
 const fs = require('fs');
 const readline = require('readline');
+const uuid = require('uuid/v4');
 const kuromoji = require('kuromoji');
 const moji = require('moji');
 const ReconnectingWebSocket = require('reconnecting-websocket');
@@ -613,18 +614,6 @@ function sendText({text, reply_id, visibility = 'public', user_id, ignoreNG = fa
         sendData.body.data.visibleUserIds = user_id;
     }
     ws.send(JSON.stringify(sendData));
-}
-
-function uuid() {
-    let uuid = '', i, random;
-    for (i = 0; i < 32; i++) {
-        random = Math.random() * 16 | 0;
-        if (i == 8 || i == 12 || i == 16 || i == 20) {
-            uuid += '-';
-        }
-        uuid += (i == 12 ? 4 : (i == 16 ? (random & 3 | 8) : random)).toString(16);
-    }
-    return uuid;
 }
 
 function getExists(text) {
