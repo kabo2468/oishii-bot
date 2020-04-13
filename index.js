c6248badab97af150d81063fcfa1ad53b64e1da8
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-04-13 18:05:37 +0900

    npm update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ if (error.length > 0) {
 
 const fs = require('fs');
 const readline = require('readline');
-const uuid = require('uuid/v4');
+const { v4: uuidv4 } = require('uuid');
 const kuromoji = require('kuromoji');
 const moji = require('moji');
 const ms = require('ms');
@@ -238,7 +238,7 @@ ws.addEventListener('message', function(data){
             ws.send(JSON.stringify({
                 type: 'api',
                 body: {
-                    id: uuid(),
+                    id: uuidv4(),
                     endpoint: 'following/create',
                     data: {
                         userId: json.body.body.id
@@ -264,7 +264,7 @@ ws.addEventListener('message', function(data){
             const reactionData = {
                 type: 'api',
                 body: {
-                    id: uuid(),
+                    id: uuidv4(),
                     endpoint: 'notes/reactions/create',
                     data: {
                         noteId: note_id,
@@ -662,7 +662,7 @@ function sendText({text, reply_id, visibility = 'public', user_id, ignoreNG = fa
     const sendData = {
         type: 'api',
         body: {
-            id: uuid(),
+            id: uuidv4(),
             endpoint: 'notes/create',
             data: {
                 visibility: visibility,
