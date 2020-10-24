bc93a5b350d9efe8dfa3708483ddbf5de9d11403
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-10-25 01:39:51 +0900

    変数とメッセージ

---
+++ src/messages.ts
@@ -0,0 +1,82 @@
+const goodText = 'おいしい';
+const badText = 'まずい';
+
+export const arrToStr = (arr: string[]): string => arr[Math.floor(Math.random() * arr.length)];
+
+export default {
+    commands: {
+        denied: 'このコマンドは、オーナーのみ実行できます。',
+        help: [
+            '/help: コマンドリストを表示する。',
+            '/ping: 生存確認する。',
+            '/info: (今のところは)DBのレコード数を表示する。',
+            '/say: なにか言わせる。(オーナーのみ)',
+            '/follow: フォローする。',
+            '/unfollow: フォローを解除する。',
+            '/delete: 削除する。（オーナーのみ）',
+            '/chart: DBのレコード数をチャートにする。（オーナーのみ）',
+        ],
+        ping: 'ぽん！',
+        nullpo: 'ガッ',
+        delete: {
+            done: (num: number): string => `${num}件削除しました。`,
+            notFound: '見つかりませんでした。',
+        },
+        follow: {
+            done: 'フォローしました。',
+            already: '既にフォローしています。',
+            cant: 'フォローできませんでした。',
+        },
+        unfollow: {
+            done: 'フォロー解除しました。',
+            not: 'フォローしていません。',
+            cant: 'フォロー解除できませんでした。',
+        },
+        chart: 'DBのレコード数です。',
+    },
+    pizza: {
+        text: [
+            {
+                name: 'ドミノ・ピザ',
+                url: 'https://www.dominos.jp/',
+            },
+            {
+                name: 'ピザーラ',
+                url: 'https://www.pizza-la.co.jp/',
+            },
+            {
+                name: 'ピザハット',
+                url: 'https://pizzahut.jp/',
+            },
+            {
+                name: 'ナポリの窯',
+                url: 'https://www.napolipizza.jp/',
+            },
+        ],
+        toText(): string {
+            let pizzaText = '';
+            this.text.forEach((shop) => (pizzaText += `?[${shop.name}](${shop.url})`));
+            return pizzaText;
+        },
+    },
+    food: {
+        good: goodText,
+        bad: badText,
+        learn: (food: string, good: string): string => `${food} は${good}\nおぼえた`,
+        search: (food: string, good: boolean): string => `${food} は${good ? goodText : badText}`,
+        hungry: (food: string, good: boolean): string => `${food} とかどう？\n${good ? goodText : badText}よ`,
+        say: (food: string, good: boolean): string => `${food}${good ? goodText : badText}`,
+        canEat: 'それ食べられる？',
+        idk: 'わからない',
+        ngWord: 'それ食べられない',
+        long: '長いもの',
+        sushi: (num: number): string => {
+            const text = [`にぎりました！`, `にぎったよ！`];
+            return `${arrToStr(text)} ${'🍣'.repeat(num)}`;
+        },
+        food: (food: string): string => {
+            const text = [`これあげる！`, `食べて！`];
+            return `${arrToStr(text)} ${food}`;
+        },
+    },
+};
