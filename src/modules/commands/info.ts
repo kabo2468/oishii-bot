39a853a831d0d568a80384a36bf6f8ef6e7f0732
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2020-10-25 03:13:03 +0900

    Infoコマンドを改良

--- src/modules/commands/info.ts
+++ src/modules/commands/info.ts
@@ -1,3 +1,5 @@
+import { readFileSync } from 'fs';
+import ms from 'ms';
 import { Bot } from '../../bot';
 import { Note } from '../../misskey/note';
 import Module from '../../module';
@@ -7,13 +9,35 @@ export default class extends Module {
     Regex = /^\/info$/i;
 
     async Run(bot: Bot, note: Note): Promise<void> {
+        const text: string[] = [];
+
         const res = await bot.runQuery<string>({ text: 'SELECT learned, count(learned) FROM oishii_table GROUP BY learned' });
 
+        // Records
         const fl = res.rows[0].count;
         const tl = res.rows[1].count;
         const all = Number(fl) + Number(fl);
-        const text = `Records: ${all.toString()} (Learned: ${tl})`;
-        this.log('Text:', text);
-        note.reply(text);
+        const recordText = `Records: ${all.toString()} (Learned: ${tl})`;
+        this.log(recordText);
+        text.push(recordText);
+
+        // Commit Hash
+        const rev = readFileSync('.git/HEAD').toString();
+        const branchHash = readFileSync('.git/' + rev.substring(5).trim())
+            .toString()
+            .trim();
+        const hash = rev.indexOf(':') === -1 ? rev : branchHash;
+        const hashText = `Commit hash: ${hash.substring(0, 7)}`;
+        this.log(hashText);
+        text.push(hashText);
+
+        // uptime
+        const uptime = process.uptime();
+        const time = ms(uptime * 1000);
+        const uptimeText = `Uptime: ${time}`;
+        this.log(uptimeText);
+        text.push(uptimeText);
+
+        note.reply(text.join('\n'));
     }
 }
