bcfe8b9dcf7e50df95843bfe5a85ffc3c60f8a2e
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2021-02-21 23:52:17 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,7 +7,6 @@ module.exports = {
         "eslint:recommended",
         "plugin:@typescript-eslint/recommended",
         "prettier",
-        "prettier/@typescript-eslint"
     ],
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
