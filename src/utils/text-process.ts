59190618d3a00b082223bb77a7ffd87f64eef3a7
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2021-07-09 01:15:22 +0900

    NGワード避けする文字を消すようにした

--- src/utils/text-process.ts
+++ src/utils/text-process.ts
@@ -23,7 +23,10 @@ export class TextProcess {
     }
 
     findNGWord(ngWord: NGWord): string | undefined {
-        const _t = ngWord.excludeNGWord(this.text);
+        const _t = ngWord
+            .excludeAllowedWord(this.text)
+            .replace(/[\s!#$%&*,-./?\\^_|~、。ー×○●]/g, '') //NGワード避けする文字を消す
+            .trim();
         const ngWords = ngWord.get;
         return ngWords.find((ng) => new RegExp(ng).exec(_t));
     }
