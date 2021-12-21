dc05ba151bb179a89f2d02ea106ead29ec90a4e2
nenohi <kimutipartylove@gmail.com>
2021-12-21 15:12:23 +0900

    全員使えるコマンドとして

--- src/modules/commands/learned.ts
+++ src/modules/commands/learned.ts
@@ -8,28 +8,24 @@ export default class extends Module {
     Name = 'learned';
     Regex = /^\/learned (.+)$/i;
     LogName = 'learned';
-
+    
     async Run(bot: Bot, note: Note): Promise<void> {
         note.reaction();
-
-        if (!bot.config.ownerIds.includes(note.note.userId)) {
-            const match = note.text.match(this.Regex);
-            if (!match) return;
-            const page = note.text.replace(match[0],"").trim().match(/^[0-9]{1,2}/);
-            const res = await bot.getUserFoods(note.note.userId,page==null?0:Number.parseInt(page[0]));
-            if (res.rowCount > 0) {
-                let text = "";
-                res.rows.forEach(item=>{
-                    text += item.name + (item.good?":美味しい\n":":まずい\n");
-                });
-                note.reply({text:text});
-            } else {
-                note.reply({ text: messages.commands.notFound });
-            }
-
-            return;
+        
+        const match = note.text.match(this.Regex);
+        if (!match) return;
+        const page = note.text.replace(match[0],"").trim().match(/^[0-9]{1,2}/);
+        const res = await bot.getUserFoods(note.note.userId,page==null?0:Number.parseInt(page[0]));
+        if (res.rowCount > 0) {
+            let text = "";
+            res.rows.forEach(item=>{
+                text += item.name + (item.good?":美味しい\n":":まずい\n");
+            });
+            note.reply({text:text});
+        } else {
+            note.reply({ text: messages.commands.notFound });
         }
-
-
+        return;
     }
 }
+    
