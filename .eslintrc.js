f6bafe598b019d407ff3f315a8c27c2178473e35
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2022-12-27 01:45:53 +0900

    Update eslintrc & tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,21 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
+    env: {
+        es2022: true,
+        node: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        "prettier",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 2022,
+        sourceType: 'module',
     },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-        "semi": "off",
-        "@typescript-eslint/semi": [
-            "error"
-        ],
-        "prefer-exponentiation-operator": "error",
-        "camelcase": "error",
-        "comma-spacing": "error",
-        "space-infix-ops": "error"
-    }
+    plugins: ['@typescript-eslint'],
+    rules: {
+        semi: 'off',
+        '@typescript-eslint/semi': ['error'],
+        'prefer-exponentiation-operator': 'error',
+        camelcase: 'error',
+        'comma-spacing': 'error',
+        'space-infix-ops': 'error',
+    },
 };
