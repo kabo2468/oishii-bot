0d6ab27a73795df87326005e025faa09cfe2ceaf
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2024-10-07 03:57:22 +0900

    NGワードの読み込みを同期処理に

--- src/ng-words.ts
+++ src/ng-words.ts
@@ -1,5 +1,4 @@
-import fs from 'fs';
-import readline from 'readline';
+import { readFileSync } from 'fs';
 import { toHalfWidth } from './utils/to-half-width.js';
 import toHiragana from './utils/to-hiragana.js';
 
@@ -8,11 +7,12 @@ export default class NGWord {
     private ngWords: string[] = [];
 
     constructor() {
-        const rs = fs.createReadStream('ngwords.txt');
-        const rl = readline.createInterface(rs);
-        rl.on('line', (line) => {
+        console.log('NGWords Initializing...');
+        const file = readFileSync('ngwords.txt', { encoding: 'utf-8' });
+        const lines = file.split('\n');
+        for (const line of lines) {
             const word = toHiragana(line.trim().toLowerCase());
-            if (word.startsWith('#')) return;
+            if (word.startsWith('#')) continue;
             if (word.startsWith('-')) {
                 if (/な/g.test(word)) this.excludedWords.push(word.substring(1).replace(/な/g, 'にゃ'));
                 this.excludedWords.push(word.substring(1));
@@ -20,7 +20,8 @@ export default class NGWord {
                 if (/な/g.test(word)) this.ngWords.push(word.replace(/な/g, 'にゃ'));
                 this.ngWords.push(word);
             }
-        });
+        }
+        console.log('NGWords initialized.', { ngWords: this.ngWords.length, excludedWords: this.excludedWords.length });
     }
 
     find(str: string): string | undefined {
