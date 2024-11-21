71ad61515f34ad0da3c392488e4cc693876426cb
kabo2468 <28654659+kabo2468@users.noreply.github.com>
2024-11-22 00:23:16 +0900

    mfm-jsを使ってURLとかメンションを消すように

--- src/misskey/note.ts
+++ src/misskey/note.ts
@@ -1,6 +1,6 @@
+import { MfmNode, extract, toString as mfmToString, parse } from 'mfm-js';
 import { Bot } from '../bot.js';
 import NGWord from '../ng-words.js';
-import { TextProcess } from '../utils/text-process.js';
 import { User } from './api.js';
 
 export interface Reactions {
@@ -30,18 +30,17 @@ export interface CreatedNote {
 }
 
 export class Note {
-    private bot: Bot;
+    readonly #bot: Bot;
     public note: CreatedNote;
-    private tp: TextProcess;
-    private _text: string;
+    #mfmNodes: MfmNode[];
+
     constructor(bot: Bot, note: CreatedNote) {
-        this.bot = bot;
+        this.#bot = bot;
         this.note = {
             ...note,
             createdAt: new Date(note.createdAt),
         };
-        this._text = note.text ?? '';
-        this.tp = new TextProcess(this._text);
+        this.#mfmNodes = parse(note.text ?? '');
     }
 
     get screenId(): string {
@@ -51,37 +50,47 @@ export class Note {
     }
 
     get text(): string {
-        return this._text;
+        return mfmToString(this.#mfmNodes).trim();
     }
 
     removeURLs(): this {
-        this._text = this.tp.removeURLs().toString();
+        this.#mfmNodes = extract(this.#mfmNodes, (node) => {
+            return !(node.type === 'url' || node.type === 'link');
+        });
         return this;
     }
 
     removeMentions(): this {
-        this._text = this.tp.removeMentions().toString();
+        this.#mfmNodes = extract(this.#mfmNodes, (node) => {
+            return node.type !== 'mention';
+        });
         return this;
     }
 
     removeMentionToMe(): this {
-        this._text = this.tp.removeMentionToMe().toString();
+        this.#mfmNodes = extract(this.#mfmNodes, (node) => {
+            return !(node.type === 'mention' && node.props.username === 'oishiibot');
+        });
         return this;
     }
 
     findNGWord(ngWord: NGWord): string | undefined {
-        return ngWord.find(this._text);
+        return ngWord.find(this.text);
     }
 
-    reply({ text, visibility = this.note.visibility, cw }: { text: string; visibility?: Visibilities; cw?: string }): void {
+    reply({
+        text,
+        visibility = this.note.visibility,
+        cw,
+    }: { text: string; visibility?: Visibilities; cw?: string }): void {
         const _t = `${this.screenId}\n${text}`;
-        this.bot.api.postText({ text: _t, visibility, replyId: this.note.id, cw }).catch((err) => {
+        this.#bot.api.postText({ text: _t, visibility, replyId: this.note.id, cw }).catch((err) => {
             throw err;
         });
     }
 
     reaction(reaction = '🍮'): void {
-        this.bot.api.reactionToNote(this.note.id, reaction).catch((err) => {
+        this.#bot.api.reactionToNote(this.note.id, reaction).catch((err) => {
             throw err;
         });
     }
